6073de4309a9d4efba707eb6a7ca82a071e7140f
kartikeya72001 <kartikeya72001@gmail.com>
2020-09-10 00:15:43 +0530

    Moosebat

--- src/js/jquery.js
+++ src/js/jquery.js
@@ -10176,11 +10176,11 @@ jQuery.parseJSON = JSON.parse;
 // AMD loader is present. jQuery is a special case. For more information, see
 // https://github.com/jrburke/requirejs/wiki/Updating-existing-libraries#wiki-anon
 
-if ( typeof define === "function" && define.amd ) {
-	define( "jquery", [], function() {
-		return jQuery;
-	} );
-}
+//if ( typeof define === "function" && define.amd ) {
+//	define( "jquery", [], function() {
+//		return jQuery;
+//	} );
+//}
 
 
 
