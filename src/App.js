71dbf60628a7e824668eb3cbf4df22ee5938a8a2
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-09-09 23:06:06 +0530

    js files imported

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
 import "./css/plugins.css";
 import "./css/style.css";
+import "./js/custom";
+import "./js/jquery";
+import "./js/plugins";
+import "./js/plugins2";
 
 class App extends Component {
    
@@ -1381,7 +1385,7 @@ class App extends Component {
 				<div class="sidebar">
 					<div class="sb-facebook sb-it">
 						<h4 class="sb-title">Find us on Facebook</h4>
-						<iframe src="" data-src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fhaintheme%2F%3Ffref%3Dts&tabs=timeline&width=300&height=315px&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=true&appId" width="300" height="315" style="border:none;overflow:hidden" ></iframe>
+						<iframe src="" data-src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fhaintheme%2F%3Ffref%3Dts&tabs=timeline&width=300&height=315px&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=true&appId" width="300" height="315" style={{border:"none",overflow:"hidden"}} ></iframe>
 					</div>
 					<div class="sb-twitter sb-it">
 						<h4 class="sb-title">Tweet to us</h4>
