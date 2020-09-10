625c20de1ea6235977d97e53f2e70d490ca5a9e6
kartikeya72001 <kartikeya72001@gmail.com>
2020-09-10 22:01:02 +0530

    Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Trending from './Trending.js';
 import Dance from './Dance.js';
+import Spotlight from './Spotlight.js';
 
 class App extends Component {
    
@@ -209,47 +210,7 @@ class App extends Component {
 				<Dance />
 				<Dance/>
 			</div>
-			
-			<div class="col-md-4">
-				<div class="sidebar">
-					<div class="ads">
-						<img src="/images/uploads/ads1.png" alt="" width="336" height="296" />
-					</div>
-					<div class="celebrities">
-						<h4 class="sb-title">Spotlight Celebrities</h4>
-						<div class="celeb-item">
-							<a href="#"><img src="/images/uploads/ava1.jpg" alt="" width="70" height="70" /></a>
-							<div class="celeb-author">
-								<h6><a href="#">Samuel N. Jack</a></h6>
-								<span>Actor</span>
-							</div>
-						</div>
-						<div class="celeb-item">
-							<a href="#"><img src="/images/uploads/ava2.jpg" alt="" width="70" height="70" /></a>
-							<div class="celeb-author">
-								<h6><a href="#">Benjamin Carroll</a></h6>
-								<span>Actor</span>
-							</div>
-						</div>
-						<div class="celeb-item">
-							<a href="#"><img src="/images/uploads/ava3.jpg" alt="" width="70" height="70" /></a>
-							<div class="celeb-author">
-								<h6><a href="#">Beverly Griffin</a></h6>
-								<span>Actor</span>
-							</div>
-						</div>
-						<div class="celeb-item">
-							<a href="#"><img src="/images/uploads/ava4.jpg" alt="" width="70" height="70" /></a>
-							<div class="celeb-author">
-								<h6><a href="#">Justin Weaver</a></h6>
-								<span>Actor</span>
-							</div>
-						</div>
-						<a href="#" class="btn">See all celebrities<i class="ion-ios-arrow-right"></i></a>
-					</div>
-				</div>
-				
-			</div>
+			<Spotlight/>
 		</div>
 	</div>
 </div>
---
+++ src/Spotlight.js
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+
+class Spotlight extends Component {
+   
+  constructor(props){
+    super(props);
+    this.state =[
+      
+    ]
+  }
+
+  render(){
+    return (
+      <div>
+        <div class="col-md-4">
+				<div class="sidebar">
+					<div class="ads">
+						<img src="/images/uploads/ads1.png" alt="" width="336" height="296" />
+					</div>
+					<div class="celebrities">
+						<h4 class="sb-title">Spotlight Celebrities</h4>
+						<div class="celeb-item">
+							<a href="#"><img src="/images/uploads/ava1.jpg" alt="" width="70" height="70" /></a>
+							<div class="celeb-author">
+								<h6><a href="#">Samuel N. Jack</a></h6>
+								<span>Actor</span>
+							</div>
+						</div>
+						<div class="celeb-item">
+							<a href="#"><img src="/images/uploads/ava2.jpg" alt="" width="70" height="70" /></a>
+							<div class="celeb-author">
+								<h6><a href="#">Benjamin Carroll</a></h6>
+								<span>Actor</span>
+							</div>
+						</div>
+						<div class="celeb-item">
+							<a href="#"><img src="/images/uploads/ava3.jpg" alt="" width="70" height="70" /></a>
+							<div class="celeb-author">
+								<h6><a href="#">Beverly Griffin</a></h6>
+								<span>Actor</span>
+							</div>
+						</div>
+						<div class="celeb-item">
+							<a href="#"><img src="/images/uploads/ava4.jpg" alt="" width="70" height="70" /></a>
+							<div class="celeb-author">
+								<h6><a href="#">Justin Weaver</a></h6>
+								<span>Actor</span>
+							</div>
+						</div>
+						<a href="#" class="btn">See all celebrities<i class="ion-ios-arrow-right"></i></a>
+					</div>
+				</div>
+				
+			</div>
+      </div>
+    )
+  }
+}
+
+export default Spotlight;
