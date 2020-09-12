d6d293fe528eba5e6d3cfbb13e1f3df299e595a5
kartikeya72001 <kartikeya72001@gmail.com>
2020-09-12 17:42:45 +0530

    Text

--- src/App.js
+++ src/App.js
@@ -24,10 +24,10 @@ class App extends Component {
         <div class="topPoster2">
             <img src="/images/blackRedTall.webp" style={{maxWidth: "100%", height: "auto"}} />
         </div>
-        <Trending title="Dance"/>
-        <Trending title="Music"/>
-        <Trending title="Acting"/>
-        <Trending title="Rapping"/>
+        <Trending title="Dance" text="Dance is a performing art form consisting of purposefully selected sequences of human movement."/>
+        <Trending title="Music" text="Music is an art form, and cultural activity, whose medium is sound."/>
+        <Trending title="Acting" text="Acting is an activity in which a story is told by means of its enactment by an actor"/>
+        <Trending title="Rapping" text="Rapping is a musical form of vocal delivery that incorporates rhyme and rhythmic speech"/>
 <div class="movie-items">
 	<div class="container">
 		<div class="row ipad-width">
--- src/Trending.js
+++ src/Trending.js
@@ -16,8 +16,11 @@ class Trending extends Component {
 	<div class="container">
 		<div class="row">
 			<div class="title-hd">
-	<h2 style={{textAlign:"left",position:"relative",left:"4rem"}}>{this.props.title}</h2>
+	<h2 style={{textAlign:"left",position:"relative",left:"3rem"}}>{this.props.title}</h2>
 			</div>
+            <div>
+            <h6 class="title-hd" style={{textAlign:"left",position:"relative",left:"3rem"}}>{this.props.text}</h6>
+            </div>
 	    	<div  class="slick-multiItemSlider">
 	    		<div class="movie-item">
 	    			<div class="mv-img">
