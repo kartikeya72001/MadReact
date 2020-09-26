545adc6f9ed77dc01d8f65bbf421dacc594be399
kartikeya72001 <kartikeya72001@gmail.com>
2020-09-26 22:35:54 +0530

    COntactUsInBetween

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import Navbar from '../components/Navbar';
+import {Container,Col,Row} from 'react-bootstrap';
+ 
 
 class About extends Component{
     constructor(props){
@@ -11,7 +13,26 @@ class About extends Component{
     }
     render(){
         return(
-            <Navbar></Navbar>
+            <div>
+                <Navbar></Navbar>
+                <Container>
+                <Row>
+                    <Col sm={7}><img src="/images/AboutBackground.png"/></Col>
+                    <Col sm={5}>
+                        <h1>About Music Art Dance</h1>
+                        <p>
+                        My Talent Hunt is an initiative of Lamlord Ventures Private Limited registered with Ministry of Corporate Affairs, INDIA. My Talent Hunt is an interactive, talent discovery platform which allows users to represent their hyper local communities by showcasing their talent across categories in a fun way. The platform enables regional users to express their talent in the best possible way. It also helps create hyper local influencers across entertainment categories for people to be inspired by them. The platform pitches groups / users across categories against each other in a healthy competitive environment to bring out the best in them.
+                        </p>
+                        <p>
+                        My Talent Hunt is the platform, where users can explore talents in a wider range and connect with them. Our world is full of abilities, but not everyone is getting a chance to showcase it to the broader audience. Mainly, the talents from rural areas do not even know how to showcase their talent. Many times, they miss excellent opportunities. We make sure that you do not miss any chance anymore to perform, showcase what you are good at, and win the contest. Our millennial segment will allow us to feature those young adults who are creatively gifted and have a passion for Fine Arts, Singing, Dancing, Photography, Modelling, Short Films.
+                        </p>
+                        <p>
+                        We all know how important it is to highlight fine arts in our educational system. It helps to foster creativity in our young minds.
+                        </p>
+                    </Col>
+                </Row>
+                </Container>
+            </div>
         )
     }
 }
