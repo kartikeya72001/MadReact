3f3708e771a67ccf473b88a0a120994709dd65dc
kartikeya72001 <kartikeya72001@gmail.com>
2020-10-01 20:42:15 +0530

    Margin

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -16,12 +16,47 @@ class HowitWorks extends Component{
             <div>
                 <Navbar color="black"></Navbar>
                 <Container className="contain">
-                    <Row style={{backgroundColor:"teal"}}>
-                        <Col md={4}>
+                    <Row className="imgRow">
+                        <Col md={4} className="images">
                             <img src="/images/HowItWorks/1a.png"></img>
                         </Col>
-                        <Col md={4}></Col>
-                        <Col md={4}><img src="/images/HowItWorks/1b.png"></img></Col>
+                        <Col md={4} className="images"></Col>
+                        <Col md={4} className="images"><img src="/images/HowItWorks/1b.png"></img></Col>
+                    </Row>
+                    <Row className="imgRow">
+                        <Col md={4} className="images">
+                            <img src="/images/HowItWorks/2a.png"></img>
+                        </Col>
+                        <Col md={4} className="images"></Col>
+                        <Col md={4} className="images"><img src="/images/HowItWorks/2b.png"></img></Col>
+                    </Row>
+                    <Row className="imgRow">
+                        <Col md={4} className="images">
+                            <img src="/images/HowItWorks/3a.png"></img>
+                        </Col>
+                        <Col md={4} className="images"></Col>
+                        <Col md={4} className="images"><img src="/images/HowItWorks/3b.png"></img></Col>
+                    </Row>
+                    <Row className="imgRow">
+                        <Col md={4} className="images">
+                            <img src="/images/HowItWorks/4a.png"></img>
+                        </Col>
+                        <Col md={4} className="images"></Col>
+                        <Col md={4} className="images"><img src="/images/HowItWorks/4b.png"></img></Col>
+                    </Row>
+                    <Row className="imgRow">
+                        <Col md={4} className="images">
+                            <img src="/images/HowItWorks/5a.png"></img>
+                        </Col>
+                        <Col md={4} className="images"></Col>
+                        <Col md={4} className="images"><img src="/images/HowItWorks/5b.png"></img></Col>
+                    </Row>
+                    <Row className="imgRow">
+                        <Col md={4} className="images">
+                            <img src="/images/HowItWorks/6a.png"></img>
+                        </Col>
+                        <Col md={4} className="images"></Col>
+                        <Col md={4} className="images"><img src="/images/HowItWorks/6b.png"></img></Col>
                     </Row>
                 </Container>
             </div>
