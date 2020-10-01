267cffc6a046d07793cadb23aa1d960ff2b1765f
Nikhil-codecracker <nikhilgupta1489@gmail.com>
2020-10-01 20:53:11 +0530

    centering

--- src/pages/HowItWorks.js
+++ src/pages/HowItWorks.js
@@ -16,42 +16,42 @@ class HowitWorks extends Component{
             <div>
                 <Navbar color="black"></Navbar>
                 <Container className="contain">
-                    <Row className="imgRow">
+                    <Row className="imgRow" id="img1">
                         <Col md={4} className="images">
                             <img src="/images/HowItWorks/1a.png"></img>
                         </Col>
                         <Col md={4} className="images"></Col>
                         <Col md={4} className="images"><img src="/images/HowItWorks/1b.png"></img></Col>
                     </Row>
-                    <Row className="imgRow">
+                    <Row className="imgRow" id="img2">
                         <Col md={4} className="images">
                             <img src="/images/HowItWorks/2a.png"></img>
                         </Col>
                         <Col md={4} className="images"></Col>
                         <Col md={4} className="images"><img src="/images/HowItWorks/2b.png"></img></Col>
                     </Row>
-                    <Row className="imgRow">
+                    <Row className="imgRow" id="img3">
                         <Col md={4} className="images">
                             <img src="/images/HowItWorks/3a.png"></img>
                         </Col>
                         <Col md={4} className="images"></Col>
                         <Col md={4} className="images"><img src="/images/HowItWorks/3b.png"></img></Col>
                     </Row>
-                    <Row className="imgRow">
+                    <Row className="imgRow" id="img4">
                         <Col md={4} className="images">
                             <img src="/images/HowItWorks/4a.png"></img>
                         </Col>
                         <Col md={4} className="images"></Col>
                         <Col md={4} className="images"><img src="/images/HowItWorks/4b.png"></img></Col>
                     </Row>
-                    <Row className="imgRow">
+                    <Row className="imgRow" id="img5">
                         <Col md={4} className="images">
                             <img src="/images/HowItWorks/5a.png"></img>
                         </Col>
                         <Col md={4} className="images"></Col>
                         <Col md={4} className="images"><img src="/images/HowItWorks/5b.png"></img></Col>
                     </Row>
-                    <Row className="imgRow">
+                    <Row className="imgRow" id="img6">
                         <Col md={4} className="images">
                             <img src="/images/HowItWorks/6a.png"></img>
                         </Col>
